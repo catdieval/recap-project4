702c4a81fda7483d9c9adb46adf2b4ac89e779f6
MareikeFla <mwidderich@gmail.com>
2024-01-10 12:02:48 +0100

    fix bug

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Form } from "./Components/Form.js";
-import useState from "react";
+import { useState } from "react";
 
 function App() {
   const [activities, setActivities] = useState([]);
