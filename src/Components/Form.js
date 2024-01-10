5985f41c48fcccf62300195a1996a457a43f7050
MareikeFla <mwidderich@gmail.com>
2024-01-10 11:32:16 +0100

    add onClick event

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,13 +1,27 @@
-export function Form() {
+export function Form({ onAddActivity }) {
   return (
     <>
       <h1>Add new activity</h1>
       <form>
-        <label for="activity">Name: </label>
-        <input type="text" id="activity"></input>
-        <label for="checkbox">Good weather activity</label>
+        <label htmlFor="activity">Name: </label>
+        <input autoFocus type="text" id="activity"></input>
+        <label htmlFor="checkbox">Good weather activity</label>
         <input type="checkbox" id="checkbox"></input>
-        <button type="submit">Submit</button>
+        <button
+          type="submit"
+          onClick={(event) => {
+            event.preventDefault();
+            const data = {
+              name: event.target.form.activity.value,
+              isForGoodWeathe: event.target.form.checkbox.checked,
+            };
+            event.target.form.reset();
+            event.target.form.activity.focus();
+            // onAddActivity(data);
+          }}
+        >
+          Submit
+        </button>
       </form>
     </>
   );
